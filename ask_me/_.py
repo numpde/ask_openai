64d849f7b18c1673c33220f5879b15cb1774a1cc
RA <numpde@null.net>
2022-12-24 14:49:52 +0400

    first implementation

---
+++ ask_me/_.py
@@ -0,0 +1,28 @@
+import openai
+import inspect
+
+
+def ask(api_key, logger=print):
+    def _(f):
+        def _(*a, **kw):
+            code = inspect.getsource(f)
+            try:
+                f(*a, **kw)
+            except Exception as ex:
+                prompt = f"CODE:\n\n{code}\n\nERROR:\n\n{ex}\n\nConcise explanation:\n\n"
+
+                response = openai.Completion.create(
+                    model='text-davinci-003',
+                    prompt=prompt,
+                    api_key=api_key,
+                    max_tokens=(len(prompt) + 150),
+                )
+
+                explanation = response['choices'][0]['text']
+                logger(f"OpenAI explanation: {explanation}")
+
+                raise ex
+
+        return _
+
+    return _
---
+++ ask_me/__init__.py
@@ -0,0 +1,1 @@
+from ._ import ask
